7ed66f58dd155e0a4334b1d36ef63ad8fef918ca
amiraflow <amira.midzic@gmail.com>
2021-10-26 07:16:12 +0200

    Starting the registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,4 +10,23 @@ class RegisterController extends Controller
     public function index(){
         return view('auth.register');
     }
+
+
+    public function store(Request $request){
+        //validation
+        $this->validate($request, [
+            'name' => 'required|max:255', 
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+        dd('top');
+
+        //storing the user
+
+        //logging the user in
+
+        //redirect
+    }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,39 @@
 <div>
 
 <form action="{{route('register')}}" method="post">
+@csrf
+<div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+    @error('name')
+    
+    {{ $message }}
+
+    @enderror
 
 
+    
+  </div>
+  <div class="form-group">
+    <label for="username">Username</label>
+    <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+  </div>
+
+<div class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="password_confirmation">Repeat password</label>
+    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Repeat password">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
 
 </form>
 
